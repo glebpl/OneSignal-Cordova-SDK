3dae66389a99c3abd69e2241b4dff7b267ecfde0
glebpl <glebpl@rambler.ru>
2019-05-28 13:04:18 +0300

    Update OneSignal.java
    createNotificationChannel method added to be used in forked Cordova plugin

--- lib/src/main/java/com/onesignal/OneSignal.java
+++ lib/src/main/java/com/onesignal/OneSignal.java
@@ -49,6 +49,7 @@ import org.json.*;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Application;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.ContentValues;
 import android.content.Context;
@@ -58,6 +59,8 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
+import android.media.AudioAttributes;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -900,6 +903,40 @@ public class OneSignal {
       requiresUserPrivacyConsent = required;
    }
 
+   /**
+    * Fork: method added
+    * Creates notification channel
+    * @param channelId
+    * @param channelName
+    * @param jo
+    */
+   public static void createNotificationChannel(String channelId, String channelName, JSONObject jo) {
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+         int importance = NotificationManager.IMPORTANCE_HIGH;
+         NotificationChannel notificationChannel = new NotificationChannel(channelId, channelName, importance);
+
+         notificationChannel.enableLights(true);
+         notificationChannel.setLightColor(Color.RED);
+         notificationChannel.enableVibration(true);
+         notificationChannel.setVibrationPattern(new long[]{0, 800});
+
+         Uri soundUri = OSUtils.getSoundUri(appContext, jo.optString("sound", null));
+
+         if (soundUri != null) {
+            // Initial channel sound
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .build();
+
+            notificationChannel.setSound(soundUri, audioAttributes);
+         }
+
+         NotificationManager notificationManager = (NotificationManager)appContext.getSystemService(Context.NOTIFICATION_SERVICE);
+
+         notificationManager.createNotificationChannel(notificationChannel);
+      }
+   }
 
    /**
     * Indicates if the SDK is still waiting for the user to provide consent
